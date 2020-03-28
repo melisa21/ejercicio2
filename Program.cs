d885b8cc2cbf9951df0e2fc1a6e8d36abc1be51f
jandresleal <62621149+jandresleal@users.noreply.github.com>
2020-03-27 21:00:50 -0300

    vvv
    <caaf

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Juan Andrés Leal");
+        }
+    }
+}
